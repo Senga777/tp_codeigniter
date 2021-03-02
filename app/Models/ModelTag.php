a0600704848168a587eb03b5943e759ea2cae341
Senga777 <51907114+Senga777@users.noreply.github.com>
2021-03-02 11:47:03 +0100

    modify model tag

--- app/Models/ModelTag.php
+++ app/Models/ModelTag.php
@@ -15,25 +15,54 @@ class ModelTag extends Model {
 
     protected $table = "tag";
     protected $allowedFields = [
-        'nom'
+        'nom', 'slug'
     ];
     protected $returnType = 'App\Entities\Tag';
     protected $useTimestamps = true;
 
     /**
      * `id_tag`, `id_recette`
-     * @param type $id_recette
+     * @param int $id_recipe
      */
-    public function findAllTags($id_recette) {
-        $db      = Database::connect();
+    public function findAllTags(int $id_recipe) {
+        $db = Database::connect();
         $builder = $db->table('tag');
         $builder->select('tag.nom');
         $builder->join('tag_recette', 'tag_recette.id_tag = tag.id');
-        $builder->where('tag_recette.id_recette', $id_recette);
+        $builder->where('tag_recette.id_recette', $id_recipe);
         $query = $builder->get();
-       
-        return $query->getResult(Tag::class);
 
+        return $query->getResult(Tag::class);
+    }
+
+    /**
+     * 
+     * @param int $id_tag
+     * @param int $id_recipe
+     * @return bool
+     */
+    public function addTagForRecipe(int $id_tag, int $id_recipe) {
+        $builder = $this->db->table('tag_recette');
+        return $builder->replace(['id_recette' => $id_recipe, 'id_tag' => $id_tag]);
+    }
+
+    /**
+     * 
+     * @param string $tag_name
+     * @return int
+     */
+    public function findOrCreate(string $tag_name): int {
+        $tag = $this->where('slug', slugify($tag_name))->find();
+        if (empty($tag)) {
+            $tag = new Tag();
+            $tag->name = $tag_name;
+            $tag->slug = slugify($tag_name);
+            $this->insert($tag);
+            $id_tag = $this->getInsertID();
+        } else {
+            $id_tag = $tag->id;
+        }
+        return $id_tag;
     }
 
 }
--- tests/app/Models/ModelTagTest.php
+++ tests/app/Models/ModelTagTest.php
@@ -16,26 +16,41 @@ class ModelTagTest extends CIDatabaseTestCase {
     protected $seed = 'TestSeeder';
     protected $seedOnce = true;
     protected $basePath = 'tests/app/Database/';
+    private ModelTag $model;
+    
 
-    public function testCreateAdd() {
-        $model = new ModelTag();
-
-        $model->createAdd("Chocolat au lait", 2);
-        $criteria = [
-            'slug' => 'chocolat-au-lait',
-            'nom' => 'Chocolat au lait'
-        ];
-        $this->seeInDatabase('tag', $criteria);
-    }
+    
 
     public function testFindAllTags() {
-        $model = new ModelTag();
-        $model->findAllTags(1);
-        $this->seeNumRecords(10, 'tag_recette', ['id_recette' => 1]);
+        $this->model->findAllTags(1);
+        $this->seeNumRecords(2, 'tag_recette', ['id_recette' => 1]);
+    }
+
+    public function testFindOrCreate() {
+        //$model = new ModelTag();
+        $criteria = [
+            'nom' => 'Chocolat au lait'
+        ];
+        $this->dontSeeInDatabase('tag', $criteria);
+        $id = $this->model->findOrCreate("Chocolat au lait");
+        $this->seeInDatabase('tag', $criteria);
+
+        $tag = $this->model->find($id);
+        $this->assertEquals('Chocolat au lait', $tag->name);
+        $this->assertEquals('chocolat-au-lait', $tag->slug);
     }
 
     protected function setUp(): void {
         parent::setUp();
+        $this->model = new ModelTag();
+        $builder = $this->db->table("tag");
+        $builder->where('slug', 'chocolat-au-lait')->delete();
+    }
+
+    protected function tearDown(): void {
+        parent::tearDown();
+        $builder = $this->db->table("tag");
+        $builder->delete( ["slug" => "chocolat-au-lait"]);
     }
 
 }
